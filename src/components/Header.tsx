e51b0ff11c54f50a8aaacfe78542941332909bc7
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-05 23:38:37 +0100

    header done

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import '../styles/Header.css'
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>Main Title</li>
+                    <li>Accueil</li>
+                    <li>Profil</li>
+                    <li>Réglages</li>
+                    <li>Communauté</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
