6cfaf36e08dccd1e70976173e35c3a3ee335b226
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-06 21:41:36 +0100

    graphactivity styling

--- src/components/GraphActivity.tsx
+++ src/components/GraphActivity.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 interface props {
     userId : number
@@ -16,16 +17,16 @@ const GraphActivity = ({userId}:props) => {
 
     const baseUrl : string = "http://localhost:3000/user/"+userId+"/activity" // 12 or 18
 
-    const [activitiesDatas, setActivitiesDatas] = useState<string>()
+    const [activitiesDatas, setActivitiesDatas] = useState<Array <session>>()
 
     useEffect(() => {
         const fetchData = async () =>  {
             try{
                 const response = await fetch(baseUrl)
                 const datas = await response.json()
-                const sessions : Array <session> = datas.data.sessions
-                const sDatas : string = JSON.stringify(sessions)
-                setActivitiesDatas(sDatas)
+                /*const sessions : Array <session> = datas.data.sessions
+                const sDatas : string = JSON.stringify(sessions)*/
+                setActivitiesDatas(datas.data.sessions)
             }
             catch(error){
                 console.log(error)
@@ -37,9 +38,41 @@ const GraphActivity = ({userId}:props) => {
     }, [baseUrl])
 
     return(
-        <>
-        {activitiesDatas&&activitiesDatas}
-        </>
+            <BarChart
+            width={700}
+            height={145}
+            data={activitiesDatas}
+            barCategoryGap="54"
+            barGap="8"
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+            >
+                <CartesianGrid 
+                strokeDasharray="3 3"
+                vertical={false}
+                />
+                <XAxis />
+                <YAxis dataKey="kilogram" interval="preserveStartEnd"/>
+                <Tooltip />
+                <Legend 
+                verticalAlign="top"
+                height={14}
+                />
+                <Bar 
+                dataKey="kilogram" 
+                fill="#282D30"
+                barSize={7}
+                />
+                <Bar 
+                dataKey="calories" 
+                fill="#E60000" 
+                barSize={7}
+                />
+            </BarChart>
     )
 
 }
